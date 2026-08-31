ff78d983629d83afbaa6f7d6a35561d592249d6b
vector15-05 <252612603+vector15-05@users.noreply.github.com>
2026-08-31 19:33:31 +0530

    fixed api url issue

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_URL: process.env.API_URL ?? 'http://localhost:6767/api',
+  },
 };
 
 export default nextConfig;
--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:6767/api',
+  baseURL: process.env.API_URL ?? 'http://localhost:6767/api',
   withCredentials: true,
 });
 
